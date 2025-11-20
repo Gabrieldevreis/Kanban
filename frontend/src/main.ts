16c5a8bfd5e65e007a4109b0609f91ba6fd68c37
gabrieldevreis <gabrielreisdev@gmail.com>
2025-11-19 23:09:26 -0300

    update

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,6 +2,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+import feather from 'feather-icons';
+ 
+
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+feather.replace();
